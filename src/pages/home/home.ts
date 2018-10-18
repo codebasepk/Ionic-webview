afab957eefcd251b11798a95fcbedf9b8afd7685
Bilal Shahid <s9iper1@gmail.com>
2018-10-18 13:55:14 +0500

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser'; /* 1 */
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private iab: InAppBrowser /* 2 */) {}
+ngOnInit(){
 
-  }
+    const browser = this.iab.create('http://www.mairapid.ro/','_self',{location:'no'}); /*3*/
+
+}
 
 }
